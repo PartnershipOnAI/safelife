major bug (I think?): sum over hot actions, don't average
The effect of averaging would be that the resultant probability of
an action is always less than 1/num_actions. The term in the rectifier
would then almost always have the same sign as the advantage, so the
rectifier would be zeroed out for negative advantages and linear for
positive ones. This explains why forgetting to take the absolute value
of the advantage outside of the rectifier didn't have any noticeable
effect. This also means that the 'proximal' part of PPO wasn't really
being used at all, or it was being used with an enormous clip size.

...fixing the bug seems to break my previous training run though. Not
sure what's going on here.

agents/ppo.py
@@ -225,7 +225,7 @@ class PPO(object):
             num_actions = op.policy.shape[-1].value
         with tf.name_scope("policy_loss"):
             hot_actions = tf.one_hot(op.actions, num_actions, dtype=tf.float32)
-            a_policy = tf.reduce_mean(op.policy * hot_actions, axis=-1)
+            a_policy = tf.reduce_sum(op.policy * hot_actions, axis=-1)
             prob_diff = tf.sign(op.advantages) * (1 - a_policy / op.old_policy)[..., None]
             if self.rescale_policy_eps:
                 # Scaling the clipping by 1 - old_policy ensures that