a58839e5364c3c4167a9009a495f694650d58ad3
Peter Eckersley <peter.eckersley@gmail.com>
2020-09-25 10:51:17 -0700

    Provide the latest & greatest error messages

--- safelife/__init__.py
+++ safelife/__init__.py
@@ -17,5 +17,5 @@ except ImportError:
     raise ImportError(
         "Cannot import module 'speedups'. "
         "Make sure that the package is correctly built and compiled using, "
-        "e.g., `python3 setup.py build`."
+        "e.g., `python3 setup.py build_ext --inplace`."
     )
