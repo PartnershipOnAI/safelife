c95834976f717bae75cbb507b27dec232f6bd01b
Peter Eckersley <peter.eckersley@gmail.com>
2021-05-03 17:15:01 -0700

    More sweeps, now with mppo3

--- gcloud/make-venv.sh
+++ gcloud/make-venv.sh
@@ -3,5 +3,5 @@
 #PATH=/usr/local/cuda/bin:/opt/anaconda3/bin/:/opt/conda/bin:/opt/conda/condabin:$PATH
 source /etc/profile
 python3 -m pip install virtualenv
-python3 -m virtualenv ~/safelife-venv --system-site-packages
+python3 -m virtualenv ~/safelife-venv
 
